e8d89956c3f2de65af48dd09e1d77be21441f890
fernando <fernandocto.scz@gmail.com>
2020-04-12 18:46:10 -0400

    [ADD] added login (query)

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,4 +1,5 @@
 import { IResolvers } from 'graphql-tools';
+import { returnResultUser } from '../lib/utils';
 
 const query: IResolvers = {
   Query: {
@@ -8,6 +9,18 @@ const query: IResolvers = {
       } catch (error) {
         console.log(error);
       }
+    },
+    async login(_: void, { email, password }, { db }): Promise<any> {
+      return await db.collection('users').findOne({ email, password })
+        .then((user: any) => {
+          if (!user) {
+            return returnResultUser(null, false, 'email and password incorrect');
+          }
+          return returnResultUser(user, true, 'successful login');
+        })
+        .catch((err: any) => {
+          return returnResultUser(null, false, 'login failed');
+        });
     },
   },
 };
