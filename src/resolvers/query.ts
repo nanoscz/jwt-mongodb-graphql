462abbd36c1599fe81e29af4fbf7357867b88f6a
fernando <fernandocto.scz@gmail.com>
2020-04-12 18:16:52 -0400

    [ADD] added users query

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -2,7 +2,13 @@ import { IResolvers } from 'graphql-tools';
 
 const query: IResolvers = {
   Query: {
-
+    async users(_: void, { }, { db }): Promise<any> {
+      try {
+        return await db.collection('users').find().toArray();
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 
