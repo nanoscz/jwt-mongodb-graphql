0da33d744b7b3867b589e2cb4eabeb816415276f
fernando <fernandocto.scz@gmail.com>
2020-04-12 11:53:08 -0400

    [FIX] improved register return

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -3,17 +3,30 @@ import { Datetime } from '../lib/datetime';
 
 const mutation: IResolvers = {
   Mutation: {
-    async register(_: void, { user }, { db } ): Promise<any> {
-      const lastUser = await db.collection('users').find().limit(1).sort({ date: -1}).toArray();
-      if(lastUser.length === 0) {
-        user.id = 1
+    async register(_: void, { user }, { db }): Promise<any> {
+      const lastUser = await db.collection('users').find().limit(1).sort({ date: -1 }).toArray();
+      if (lastUser.length === 0) {
+        user.id = 1;
       } else {
         user.id = lastUser[0].id + 1;
       }
       user.date = new Datetime().getCurrentDateTime();
-      await db.collection('users').insertOne(user);
-      return user;
-    }
+      return db.collection('users').insertOne(user)
+        .then((data: any) => {
+          return {
+            user,
+            status: true,
+            message: 'successful register',
+          };
+        })
+        .catch((err: any) => {
+          return {
+            status: false,
+            message: 'register failed.',
+            user: null,
+          };
+        });
+    },
   },
 };
 
