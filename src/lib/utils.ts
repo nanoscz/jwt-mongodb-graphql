7fdc2153083b099cdbb9e7ffa376400daa3663be
fernando <fernandocto.scz@gmail.com>
2020-04-12 22:53:24 -0400

    [ADD] added variables (utils)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,6 +6,20 @@ function returnResultUser(user: any, status: boolean, message: string) {
   };
 }
 
+function returnResulToken(token: any, status: boolean, message: string) {
+  return {
+    token,
+    status,
+    message,
+  };
+}
+
+const TOKEN_FAIL = 'invalid token.';
+const TOKEN_SUCCESS = 'token correct.';
+
 export {
   returnResultUser,
+  returnResulToken,
+  TOKEN_FAIL,
+  TOKEN_SUCCESS,
 };
