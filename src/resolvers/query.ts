f70534a37041bb7d74050bb02538f6b08f83a82c
fernando <fernandocto.scz@gmail.com>
2020-04-12 22:58:17 -0400

    [FIX] password comparison

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,5 +1,7 @@
+import bcryptjs  from 'bcryptjs';
 import { IResolvers } from 'graphql-tools';
-import { returnResultUser } from '../lib/utils';
+import { TOKEN_FAIL, TOKEN_SUCCESS, returnResulToken, returnResultUser } from '../lib/utils';
+import JWT from '../lib/jwt';
 
 const query: IResolvers = {
   Query: {
@@ -11,16 +13,28 @@ const query: IResolvers = {
       }
     },
     async login(_: void, { email, password }, { db }): Promise<any> {
-      return await db.collection('users').findOne({ email, password })
-        .then((user: any) => {
-          if (!user) {
-            return returnResultUser(null, false, 'email and password incorrect');
-          }
-          return returnResultUser(user, true, 'successful login');
-        })
-        .catch((err: any) => {
-          return returnResultUser(null, false, 'login failed');
-        });
+      try {
+        const user = await db.collection('users').findOne({ email });
+        if (!user) {
+          return returnResulToken(null, false, 'email and password incorrect.');
+        }
+        const isEqualPassword = await bcryptjs.compareSync(password, user.password);
+        if (!isEqualPassword) {
+          return returnResulToken(null, false, 'email and password incorrect.');
+        }
+        delete user.password;
+        const token = new JWT().sign({ user });
+        return returnResulToken(token, true, 'successful login');
+      } catch (error) {
+        return returnResulToken(null, false, 'login failed');
+      }
+    },
+    me(_: void, __:any, { token }) {
+      const info: any = new JWT().verify(token);
+      if (info === TOKEN_FAIL) {
+        return returnResultUser(null, false, info);
+      }
+      return returnResultUser(info.user, true, TOKEN_SUCCESS);
     },
   },
 };
